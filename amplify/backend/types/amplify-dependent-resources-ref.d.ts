307bdd7ec72d7b8b1189c96779c719eddb86b6fa
JohnRamberger <jramberger3@gatech.edu>
2022-11-06 21:10:00 -0500

    second commit
    wowzers

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "hacklyticsportal2023b2114565": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Amplify } from "aws-amplify";
+import {
+  AmplifyProvider,
+  Authenticator,
+  Button,
+  Flex,
+  Image,
+  Text,
+  View,
+} from "@aws-amplify/ui-react";
+import aws_exports from "./aws-exports";
 
-function App() {
+import "@aws-amplify/ui-react/styles.css";
+import theme from "./theme";
+import logo from "./logo.svg";
+
+Amplify.configure(aws_exports);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AmplifyProvider theme={theme}>
+      <Authenticator>
+        {({ signOut, user }) => (
+          <Flex
+            direction="column"
+            justifyContent="flex-start"
+            alignItems="center"
+            alignContent="flex-start"
+            wrap="nowrap"
+            gap="1rem"
+            textAlign="center"
+          >
+            <View width="100%">
+              <Image src={logo} alt="logo" width={240} height={240} />
+            </View>
+
+            {user && (
+              <View width="100%">
+                <Text>Hello {user.username}</Text>
+                <Button onClick={signOut}>
+                  <Text>Sign Out</Text>
+                </Button>
+              </View>
+            )}
+
+            <View width="100%">
+              <Text>
+                Edit <code>src/App.tsx</code> and save to reload.
+              </Text>
+            </View>
+          </Flex>
+        )}
+      </Authenticator>
+    </AmplifyProvider>
   );
-}
+};
 
 export default App;

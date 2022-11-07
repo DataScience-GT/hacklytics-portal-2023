7ba9c093f6b2522c9d8508afde8fb11d3b31510c
JohnRamberger <jramberger3@gatech.edu>
2022-11-07 00:13:42 -0500

    Update HomePage.tsx
    changed email to name

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from 'react';
 import styles from './HomePage.module.scss';
 
-import { Amplify } from "aws-amplify";
 import {
   View,
   Text,
@@ -20,7 +19,7 @@ const HomePage: FC<HomePageProps> = ({user, signOut}) => (
   <div className={styles.HomePage}>
     {user && (
               <View width="100%">
-                <Text>Hello {user.attributes?.email}</Text>
+                <Text>Hello {user.attributes?.name}</Text>
                 <Button onClick={signOut}>
                   <Text>Sign Out</Text>
                 </Button>

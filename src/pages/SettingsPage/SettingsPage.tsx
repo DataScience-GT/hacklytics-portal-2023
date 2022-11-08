2172f90507a458ff9fa9d4e583dd921ce4db0956
JohnRamberger <jramberger3@gatech.edu>
2022-11-08 09:14:40 -0500

    Update SettingsPage.tsx

--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -1,13 +1,17 @@
 import React, { FC, useContext, useEffect, useState } from "react";
 import styles from "./SettingsPage.module.scss";
+import GLOBAL from "../../GLOBAL.module.scss";
 
 import { AmplifyUser, AuthEventData } from "@aws-amplify/ui";
+import { Auth } from "aws-amplify";
 
 import {
   ToggleButtonGroup,
   ToggleButton,
   Text,
   View,
+  Button,
+  Flex,
 } from "@aws-amplify/ui-react";
 import { CognitoUserSession } from "amazon-cognito-identity-js";
 
@@ -20,46 +24,62 @@ interface SettingsPageProps {
 
 const SettingsPage: FC<SettingsPageProps> = ({ user, signOut }) => {
   const { theme, setTheme, colorMode, setColorMode } = useContext(ThemeContext);
-  useEffect(() => {
-    console.log(user)
-    user?.listDevices(10, null, {
-      onSuccess: (result) => {
-        console.log(result);
-      },
-      onFailure: (err) => {
-        console.log(err);
-      }
-    })
-  }, []);
   return (
     <div className={styles.SettingsPage}>
       <View width="100%" padding="1em">
-        <ToggleButtonGroup
-          value={theme}
-          isExclusive
-          onChange={(value) => setTheme(value.toString() as Theme)}
-        >
-          {Object.values(Theme).map((t, i) => (
-            <ToggleButton
-              key={i}
-              value={t}
-              style={{ textTransform: "capitalize" }}
+        <Flex direction="column" gap={"1em"}>
+          <View>
+            <ToggleButtonGroup
+              value={theme}
+              isExclusive
+              onChange={(value) => setTheme(value.toString() as Theme)}
             >
-              {t}
-            </ToggleButton>
-          ))}
-        </ToggleButtonGroup>
-        <ToggleButtonGroup
-          value={colorMode}
-          isExclusive
-          onChange={(value) =>
-            setColorMode(value.toString() as "system" | "light" | "dark")
-          }
-        >
-          <ToggleButton value="light">Light</ToggleButton>
-          <ToggleButton value="dark">Dark</ToggleButton>
-          <ToggleButton value="system">System</ToggleButton>
-        </ToggleButtonGroup>
+              {Object.values(Theme).map((t, i) => (
+                <ToggleButton
+                  key={i}
+                  value={t}
+                  style={{ textTransform: "capitalize" }}
+                >
+                  {t}
+                </ToggleButton>
+              ))}
+            </ToggleButtonGroup>
+          </View>
+          <View>
+            <ToggleButtonGroup
+              value={colorMode}
+              isExclusive
+              onChange={(value) =>
+                setColorMode(value.toString() as "system" | "light" | "dark")
+              }
+            >
+              <ToggleButton value="light">Light</ToggleButton>
+              <ToggleButton value="dark">Dark</ToggleButton>
+              <ToggleButton value="system">System</ToggleButton>
+            </ToggleButtonGroup>
+          </View>
+          <View>
+            <Button
+              className={GLOBAL.Danger}
+              onClick={() => {
+                //global sign out
+                Auth.signOut({ global: true });
+              }}
+              variation="primary"
+            >
+              Global Logout
+            </Button>
+          </View>
+          <View>
+            <Button
+              className={GLOBAL.Danger}
+              onClick={signOut}
+              variation="primary"
+            >
+              Logout
+            </Button>
+          </View>
+        </Flex>
       </View>
     </div>
   );

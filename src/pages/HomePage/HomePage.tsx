28b2f38e33dc09799654eddb0bda9c7bfe7a4a9f
JohnRamberger <jramberger3@gatech.edu>
2022-11-08 13:31:23 -0500

    Update HomePage.tsx

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 import styles from "./HomePage.module.scss";
 
-import { View, Text, Button } from "@aws-amplify/ui-react";
+import { View, Text, Button, Collection, Card } from "@aws-amplify/ui-react";
 
 import { AmplifyUser, AuthEventData } from "@aws-amplify/ui";
 
@@ -12,19 +12,25 @@ interface HomePageProps {
 
 const HomePage: FC<HomePageProps> = ({ user, signOut }) => (
   <div className={styles.HomePage}>
-    {user && (
-      <View width="100%" padding="1em">
-        <Text>dev Hello {user.attributes?.name}</Text>
-        {user.attributes &&
-          Object.values(user.attributes).map((attr, i) => (
-            <Text key={i}>{attr}</Text>
-          ))}
-        <Button onClick={signOut}>
-          <Text>Sign Out</Text>
-        </Button>
-      </View>
-    )}
+    <View width="100%" padding="medium">
+        <Card variation="elevated" maxWidth={"20em"}>
+          <Text>a</Text>
+        </Card>
+    </View>
   </div>
 );
+
+// {user && (
+//   <View width="100%" padding="1em">
+//     <Text>dev Hello {user.attributes?.name}</Text>
+//     {user.attributes &&
+//       Object.values(user.attributes).map((attr, i) => (
+//         <Text key={i}>{attr}</Text>
+//       ))}
+//     <Button onClick={signOut}>
+//       <Text>Sign Out</Text>
+//     </Button>
+//   </View>
+// )}
 
 export default HomePage;

fb82bc23dbf33d2e6627d022847d168fec159c57
JohnRamberger <jramberger3@gatech.edu>
2022-11-08 01:26:04 -0500

    Update SettingsPage.tsx
    includes Themes

--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -1,9 +1,11 @@
-import React, { FC, useState } from "react";
+import React, { FC, useContext, useState } from "react";
 import styles from "./SettingsPage.module.scss";
 
 import { AmplifyUser, AuthEventData } from "@aws-amplify/ui";
 
 import { ToggleButtonGroup, ToggleButton, Text } from "@aws-amplify/ui-react";
+
+import { Theme, ThemeContext } from "../../context/ThemeContext";
 
 interface SettingsPageProps {
   user?: AmplifyUser;
@@ -11,19 +13,35 @@ interface SettingsPageProps {
 }
 
 const SettingsPage: FC<SettingsPageProps> = ({ user, signOut }) => {
-  const [colorMode, setColorMode] = useState("system");
+  const { theme, setTheme, colorMode, setColorMode } = useContext(ThemeContext);
   return (
     <div className={styles.SettingsPage}>
       <ToggleButtonGroup
+        value={theme}
+        isExclusive
+        onChange={(value) => setTheme(value.toString() as Theme)}
+      >
+        {Object.values(Theme).map((t, i) => (
+          <ToggleButton
+            key={i}
+            value={t}
+            style={{ textTransform: "capitalize" }}
+          >
+            {t}
+          </ToggleButton>
+        ))}
+      </ToggleButtonGroup>
+      <ToggleButtonGroup
         value={colorMode}
         isExclusive
-        onChange={(value) => setColorMode(value.toString())}
+        onChange={(value) =>
+          setColorMode(value.toString() as "system" | "light" | "dark")
+        }
       >
         <ToggleButton value="light">Light</ToggleButton>
         <ToggleButton value="dark">Dark</ToggleButton>
         <ToggleButton value="system">System</ToggleButton>
       </ToggleButtonGroup>
-      <Text>Color Mode: {colorMode}</Text>
     </div>
   );
 };

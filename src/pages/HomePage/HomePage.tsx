b26bd4cb961ea66ba6a805b0cc5a6e2ea8726d7d
JohnRamberger <jramberger3@gatech.edu>
2022-11-07 23:46:24 -0500

    Update HomePage.tsx
    added navbar

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,6 +4,7 @@ import styles from "./HomePage.module.scss";
 import { View, Text, Button } from "@aws-amplify/ui-react";
 
 import { AmplifyUser, AuthEventData } from "@aws-amplify/ui";
+import Navbar from "../../components/Navbar/Navbar";
 
 interface HomePageProps {
   user?: AmplifyUser;
@@ -12,6 +13,7 @@ interface HomePageProps {
 
 const HomePage: FC<HomePageProps> = ({ user, signOut }) => (
   <div className={styles.HomePage}>
+    <Navbar user={user} signOut={signOut}/>
     {user && (
       <View width="100%">
         <Text>Hello {user.attributes?.name}</Text>

78cd66e697bab42b3c257018b32b1447a271ec37
JohnRamberger <jramberger3@gatech.edu>
2022-11-08 00:50:51 -0500

    Update SettingsPage.tsx

--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -1,17 +1,31 @@
-import React, { FC } from 'react';
-import styles from './SettingsPage.module.scss';
+import React, { FC, useState } from "react";
+import styles from "./SettingsPage.module.scss";
 
 import { AmplifyUser, AuthEventData } from "@aws-amplify/ui";
+
+import { ToggleButtonGroup, ToggleButton, Text } from "@aws-amplify/ui-react";
 
 interface SettingsPageProps {
   user?: AmplifyUser;
   signOut?: (data?: AuthEventData | undefined) => void;
 }
 
-const SettingsPage: FC<SettingsPageProps> = ({user, signOut}) => (
-  <div className={styles.SettingsPage}>
-    SettingsPage Component
-  </div>
-);
+const SettingsPage: FC<SettingsPageProps> = ({ user, signOut }) => {
+  const [colorMode, setColorMode] = useState("system");
+  return (
+    <div className={styles.SettingsPage}>
+      <ToggleButtonGroup
+        value={colorMode}
+        isExclusive
+        onChange={(value) => setColorMode(value.toString())}
+      >
+        <ToggleButton value="light">Light</ToggleButton>
+        <ToggleButton value="dark">Dark</ToggleButton>
+        <ToggleButton value="system">System</ToggleButton>
+      </ToggleButtonGroup>
+      <Text>Color Mode: {colorMode}</Text>
+    </div>
+  );
+};
 
 export default SettingsPage;

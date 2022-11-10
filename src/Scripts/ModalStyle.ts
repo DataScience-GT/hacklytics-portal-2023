2a06f4042794a0a1bd452f8cb603426819453a1f
JohnRamberger <jramberger3@gatech.edu>
2022-11-10 15:04:13 -0500

    Update ModalStyle.ts

--- src/Scripts/ModalStyle.ts
+++ src/Scripts/ModalStyle.ts
@@ -1,7 +1,7 @@
 const modalStyle = {
   content: {
     background: "var(--amplify-colors-background-primary)",
-    width: "50%",
+    width: "fit-content",
     height: "fit-content",
     top: "50%",
     left: "50%",

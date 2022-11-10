ce1c5505751b4f9fe8f3d325d2158dffef7d4ce1
JohnRamberger <jramberger3@gatech.edu>
2022-11-10 15:00:14 -0500

    Updated AdminPage
    updated admin settings

--- src/pages/AdminPage/AdminPage.tsx
+++ src/pages/AdminPage/AdminPage.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useEffect } from "react";
 import styles from "./AdminPage.module.scss";
+import GLOBAL from "../../GLOBAL.module.scss";
 import Modal from "react-modal";
 
 import { AmplifyUser, AuthEventData } from "@aws-amplify/ui";
@@ -9,32 +10,53 @@ import {
   SwitchField,
   Button,
   Alert,
+  Flex,
+  Divider,
+  Collection,
+  Tabs,
+  TabItem,
 } from "@aws-amplify/ui-react";
 import modalStyle from "../../Scripts/ModalStyle";
 import Status from "../../Types/Status";
 import StatusAlert from "../../components/StatusAlert/StatusAlert";
+import { Link } from "react-router-dom";
 
 interface AdminPageProps {
   user?: AmplifyUser;
   signOut?: (data?: AuthEventData | undefined) => void;
 }
 
+const SettingTabMap = new Map<string, number>([
+  ["/admin/settings/general", 0],
+  ["/admin/settings/event", 1],
+]);
+
 const AdminPage: FC<AdminPageProps> = ({ user, signOut }) => {
   // settings --------------------
   const [settingsModalOpen, setSettingsModalOpen] = React.useState(
-    window.location.pathname === "/admin/settings"
+    window.location.pathname.includes("/admin/settings")
   );
+
   const [status, setStatus] = React.useState<Status>({
     show: false,
-    type: "error",
-    message: "asd asd asd ",
   });
 
   const [eventActive, setEventActive] = React.useState(false);
 
-  const saveSettings = (e: React.ChangeEvent): boolean => {
+  const saveSettings = (e: React.ChangeEvent, checked: boolean) => {
+    setStatus({
+      show: false,
+      message: "",
+    });
     console.log(e.currentTarget);
-    return false;
+    if (checked) {
+      setStatus({
+        show: true,
+        type: "success",
+        message: "Settings saved",
+        isDismissible: true,
+      });
+    }
   };
 
   return (
@@ -60,16 +82,62 @@ const AdminPage: FC<AdminPageProps> = ({ user, signOut }) => {
           parentSelector={() => document.getElementById("modal-container")!}
           style={modalStyle}
         >
-          <Heading level={5}>Admin Settings</Heading>
-          <SwitchField
-            label="Event Open?"
-            isChecked={eventActive}
-            onChange={(e) => {
-              setEventActive(e.currentTarget.checked);
-              saveSettings(e);
-            }}
-          />
-          <StatusAlert status={status} />
+          <Flex
+            direction="column"
+            justifyContent={"space-between"}
+          >
+            <Tabs
+              spacing="relative"
+              defaultIndex={SettingTabMap.get(window.location.pathname) ?? 0}
+              grow={1}
+            >
+              <TabItem
+                title="General"
+                onClick={() => {
+                  window.history.pushState(
+                    {},
+                    "Admin Settings",
+                    "/admin/settings/general"
+                  );
+                }}
+              >
+                <View padding="medium 0 0 0">
+                  <Heading level={5}>Admin Settings</Heading>
+                  <SwitchField
+                    label="Event Open?"
+                    isChecked={eventActive}
+                    onChange={(e) => {
+                      setEventActive(e.currentTarget.checked);
+                      saveSettings(e, e.currentTarget.checked);
+                    }}
+                  />
+                  <StatusAlert status={status} />
+                </View>
+              </TabItem>
+              <TabItem
+                title="Event"
+                onClick={() => {
+                  window.history.pushState(
+                    {},
+                    "Event Settings",
+                    "/admin/settings/event"
+                  );
+                }}
+              >
+                asdasd
+              </TabItem>
+            </Tabs>
+            <Flex direction={"row"} justifyContent="flex-end">
+              <Button
+                onClick={() => {
+                  window.history.pushState({}, "Admin", "/admin");
+                  setSettingsModalOpen(false);
+                }}
+              >
+                Close
+              </Button>
+            </Flex>
+          </Flex>
         </Modal>
       </View>
     </div>

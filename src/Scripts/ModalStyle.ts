373c75b88b93ee9f3af4fe77ac8a8815ac375b75
JohnRamberger <jramberger3@gatech.edu>
2022-11-10 13:58:33 -0500

    Create ModalStyle.ts

---
+++ src/Scripts/ModalStyle.ts
@@ -0,0 +1,14 @@
+const modalStyle = {
+  content: {
+    background: "var(--amplify-colors-background-primary)",
+    width: "50%",
+    height: "fit-content",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    maxWidth: "80%",
+    maxHeight: "100%",
+  },
+};
+
+export default modalStyle;

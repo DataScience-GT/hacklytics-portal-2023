e3852bf8d3204ca4acb45c3358db90f70aacde18
JohnRamberger <jramberger3@gatech.edu>
2022-11-10 12:14:07 -0500

    Update Navbar.tsx
    added Admin Console button for admins

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import styles from "./Navbar.module.scss";
 
 import { AmplifyUser, AuthEventData } from "@aws-amplify/ui";
@@ -11,6 +11,7 @@ import {
   Menu,
   MenuItem,
   Divider,
+  Button,
 } from "@aws-amplify/ui-react";
 
 import { Link } from "react-router-dom";
@@ -22,71 +23,90 @@ interface NavbarProps {
   signOut?: (data?: AuthEventData | undefined) => void;
 }
 
-const Navbar: FC<NavbarProps> = ({ user, signOut }) => (
-  <div data-testid="Navbar">
-    <View as="header" className={styles.Navbar} width="100vw" padding="medium">
-      <Flex
-        direction={"row"}
-        justifyContent="space-between"
-        alignItems="center"
-        gap="0.5em"
-        height="100%"
-        width="100%"
+const Navbar: FC<NavbarProps> = ({ user, signOut }) => {
+  const [groups, setGroups] = useState<string[]>([]);
+  useEffect(() => {
+    const g = user?.getSignInUserSession()?.getAccessToken().payload[
+      "cognito:groups"
+    ];
+    if (g) setGroups(g);
+  }, []);
+  return (
+    <div data-testid="Navbar">
+      <View
+        as="header"
+        className={styles.Navbar}
+        width="100vw"
+        padding="medium"
       >
         <Flex
           direction={"row"}
-          justifyContent="flex-start"
+          justifyContent="space-between"
           alignItems="center"
           gap="0.5em"
           height="100%"
-          width="fit-content"
+          width="100%"
         >
-          <Link to="/" style={{ textDecoration: "none" }}>
-            <Flex
-              direction={"row"}
-              justifyContent="flex-start"
-              alignItems="center"
-              gap="0.5em"
-              height="100%"
-              width="fit-content"
-            >
-              <Image className={styles.Logo} alt="DSGT Logo" src={logo} />
-              <Heading className={styles.DSGT} level={4}>
-                DSGT
-              </Heading>
-            </Flex>
-          </Link>
+          <Flex
+            direction={"row"}
+            justifyContent="flex-start"
+            alignItems="center"
+            gap="0.5em"
+            height="100%"
+            width="fit-content"
+          >
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <Flex
+                direction={"row"}
+                justifyContent="flex-start"
+                alignItems="center"
+                gap="0.5em"
+                height="100%"
+                width="fit-content"
+              >
+                <Image className={styles.Logo} alt="DSGT Logo" src={logo} />
+                <Heading className={styles.DSGT} level={4}>
+                  DSGT
+                </Heading>
+              </Flex>
+            </Link>
+          </Flex>
+          <Flex
+            direction={"row"}
+            justifyContent="flex-end"
+            alignItems="center"
+            gap="1em"
+            height="100%"
+            width="fit-content"
+            grow={1}
+          >
+            {groups.includes("Administrator") && (
+              <Link to="/admin" style={{ textDecoration: "none" }}>
+                <Button size="small">Admin Console</Button>
+              </Link>
+            )}
+            <View width="fit-content">
+              <Menu menuAlign="end">
+                <Link className={styles.MenuLink} to="/">
+                  <MenuItem>Dashboard</MenuItem>
+                </Link>
+                <Link className={styles.MenuLink} to="/account">
+                  <MenuItem>Account</MenuItem>
+                </Link>
+                <Link className={styles.MenuLink} to="/settings">
+                  <MenuItem>Settings</MenuItem>
+                </Link>
+                <Divider />
+                <MenuItem className={styles.LogoutButton} onClick={signOut}>
+                  Logout
+                </MenuItem>
+              </Menu>
+            </View>
+          </Flex>
         </Flex>
-        <Flex
-          direction={"row"}
-          justifyContent="flex-end"
-          alignItems="center"
-          gap="0.5em"
-          height="100%"
-          width="fit-content"
-          grow={1}
-        >
-          <View width="fit-content">
-            <Menu menuAlign="end">
-              <Link className={styles.MenuLink} to="/">
-                <MenuItem>Dashboard</MenuItem>
-              </Link>
-              <Link className={styles.MenuLink} to="/account">
-                <MenuItem>Account</MenuItem>
-              </Link>
-              <Link className={styles.MenuLink} to="/settings">
-                <MenuItem>Settings</MenuItem>
-              </Link>
-              <Divider />
-              <MenuItem className={styles.LogoutButton} onClick={signOut}>
-                Logout
-              </MenuItem>
-            </Menu>
-          </View>
-        </Flex>
-      </Flex>
-    </View>
-  </div>
-);
+      </View>
+    </div>
+  );
+};
 
 export default Navbar;

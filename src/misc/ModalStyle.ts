557bb076ba2464cb910568e2c11cdf3db9506533
JohnRamberger <jramberger3@gatech.edu>
2022-11-13 12:22:29 -0500

    Update ModalStyle.ts

--- src/misc/ModalStyle.ts
+++ src/misc/ModalStyle.ts
@@ -12,4 +12,19 @@ const modalStyle = {
   },
 };
 
+const modalFormStyle = {
+  content: {
+    background: "var(--amplify-colors-background-primary)",
+    width: "40%",
+    height: "fit-content",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    maxWidth: "100%",
+    maxHeight: "100%",
+    minWidth: "300px",
+  },
+};
+
 export default modalStyle;
+export { modalFormStyle };

18b4dd2d15509d0a0d0353914e4b5b18f71a5b83
JohnRamberger <jramberger3@gatech.edu>
2022-11-12 23:09:14 -0500

    Update AdminPage.tsx

--- src/pages/AdminPage/AdminPage.tsx
+++ src/pages/AdminPage/AdminPage.tsx
@@ -109,7 +109,10 @@ const AdminPage: FC<AdminPageProps> = ({ user, signOut }) => {
     const res: any = await API.graphql({
       query: getAdminSettings,
       variables: {
-        id: "9996afdb-c7e7-46fc-bfae-b0939b9027d0",
+        id:
+          process.env.REACT_APP_HACKLYTICS_ADMIN_SETTINGS_ID ||
+          ""
+          // "9996afdb-c7e7-46fc-bfae-b0939b9027d0",
       },
       authMode: "AMAZON_COGNITO_USER_POOLS",
     });

b8b761dca2e66afaf1472e9d7215c45a8c5358ad
JohnRamberger <jramberger3@gatech.edu>
2023-01-28 17:58:29 -0500

    Update CheckpointPage.tsx

--- src/pages/CheckpointPage/CheckpointPage.tsx
+++ src/pages/CheckpointPage/CheckpointPage.tsx
@@ -4,8 +4,9 @@ import styles from "./CheckpointPage.module.scss";
 import { AmplifyUser, AuthEventData } from "@aws-amplify/ui";
 import { View, Text } from "@aws-amplify/ui-react";
 import { useParams } from "react-router-dom";
-import { getScavengerHunt } from "../../graphql";
+import { getScavengerHunt, listScavengerHuntCheckins } from "../../graphql";
 import { API } from "aws-amplify";
+import { ScavengerHuntCheckin } from "../../models";
 
 interface CheckpointPageProps {
   user?: AmplifyUser;
@@ -28,11 +29,26 @@ const CheckpointPage: FC<CheckpointPageProps> = ({ user, signOut }) => {
     }
     // get checkpoint from db
     var checkpoint: any = await API.graphql({
+      query: listScavengerHuntCheckins,
+      variables: { userId: user?.username },
+    });
+    var oldCheckpoints = checkpoint.data.listScavengerHuntCheckins.items;
+    var currentCheckpoints = oldCheckpoints.filter(
+      (c: ScavengerHuntCheckin) =>
+        c.checkpointID === checkpointId && c.userID === user?.username
+    );
+
+    if (currentCheckpoints.length > 0) {
+      // redirect to home page
+      window.location.href = "/";
+      return;
+    }
+
+    // create new checkpoint
+    var hunt: any = await API.graphql({
       query: getScavengerHunt,
       variables: { id: checkpointId },
     });
-
-    console.log(checkpoint);
   };
 
   return (

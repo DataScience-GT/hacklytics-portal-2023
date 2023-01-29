608e964e5f185e5fdf1f01d311d31fe3570c3309
JohnRamberger <jramberger3@gatech.edu>
2023-01-29 00:29:13 -0500

    Update Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -74,13 +74,13 @@ const Navbar: FC<NavbarProps> = ({ user, signOut }) => {
             width="fit-content"
             grow={1}
           >
-            {user &&
+            {/* {user &&
               (getGroups(user).includes("Scavenger") ||
                 getGroups(user).includes("Administrator")) && (
                 <Link to="/scavengerhunts" style={{ textDecoration: "none" }}>
                   <Button size="small">Scavenger Hunts</Button>
                 </Link>
-              )}
+              )} */}
             {user && getGroups(user).includes("Administrator") && (
               <Link to="/admin" style={{ textDecoration: "none" }}>
                 <Button size="small">Admin Console</Button>
@@ -91,6 +91,13 @@ const Navbar: FC<NavbarProps> = ({ user, signOut }) => {
                 <Link className={styles.MenuLink} to="/">
                   <MenuItem>Dashboard</MenuItem>
                 </Link>
+                {user &&
+                  (getGroups(user).includes("Scavenger") ||
+                    getGroups(user).includes("Administrator")) && (
+                    <Link className={styles.MenuLink} to="/scavengerhunts">
+                      <MenuItem>Scavenger Hunts</MenuItem>
+                    </Link>
+                  )}
                 <Link className={styles.MenuLink} to="/account">
                   <MenuItem>Account</MenuItem>
                 </Link>

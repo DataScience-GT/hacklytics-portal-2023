f6af526bb7ffc85b32c918aa6665cc73ef8866b1
JohnRamberger <jramberger3@gatech.edu>
2023-02-08 12:23:45 -0500

    Update Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -16,7 +16,8 @@ import {
 
 import { Link } from "react-router-dom";
 
-import logo from "../../assets/images/DSGT/square-logo.png";
+// import logo from "../../assets/images/DSGT/square-logo.png";
+import logo from "../../assets/images/Hacklytics/logo.svg";
 import getGroups from "../../misc/Groups";
 
 interface NavbarProps {
@@ -60,7 +61,7 @@ const Navbar: FC<NavbarProps> = ({ user, signOut }) => {
               >
                 <Image className={styles.Logo} alt="DSGT Logo" src={logo} />
                 <Heading className={styles.DSGT} level={4}>
-                  DSGT
+                  Hacklytics 2023
                 </Heading>
               </Flex>
             </Link>

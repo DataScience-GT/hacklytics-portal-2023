f09ebf6377a57f80763e94fb3e77f3a51fcd1e0c
Lavan Vivekanandasarma <lavan.vivek11@gmail.com>
2025-02-16 02:38:27 -0500

    Added QR Codes

--- src/pages/AccountPage/AccountPage.tsx
+++ src/pages/AccountPage/AccountPage.tsx
@@ -15,6 +15,7 @@ import {
   Heading,
 } from "@aws-amplify/ui-react";
 import { Link } from "react-router-dom";
+import { QRCodeSVG, QRCodeCanvas } from "qrcode.react";
 
 interface AccountPageProps {
   user?: AmplifyUser;
@@ -41,40 +42,65 @@ const AccountPage: FC<AccountPageProps> = ({ user, signOut }) => (
               {user.attributes?.email && (
                 <TableRow>
                   <TableCell>Email</TableCell>
-                  <TableCell>{user?.attributes?.email}</TableCell>
+                  <TableCell>{user.attributes.email}</TableCell>
                 </TableRow>
               )}
               <TableRow>
-                  <TableCell>School Email</TableCell>
-                  <TableCell>
-                  {(user?.attributes["custom:gtemail"] &&
-                    user?.attributes["custom:gtemail"]) ??
-                    "None"}
+                <TableCell>School Email</TableCell>
+                <TableCell>
+                  {user.attributes["custom:gtemail"] ?? "None"}
                 </TableCell>
-                </TableRow>
+              </TableRow>
               {user.attributes?.name && (
                 <TableRow>
                   <TableCell>Name</TableCell>
-                  <TableCell>{user?.attributes?.name}</TableCell>
+                  <TableCell>{user.attributes.name}</TableCell>
                 </TableRow>
               )}
               {user.attributes?.birthdate && (
                 <TableRow>
                   <TableCell>Birthdate</TableCell>
-                  <TableCell>{user?.attributes?.birthdate}</TableCell>
+                  <TableCell>{user.attributes.birthdate}</TableCell>
                 </TableRow>
               )}
               <TableRow>
                 <TableCell>Dietary Restriction</TableCell>
                 <TableCell>
-                  {(user?.attributes["custom:foodPreference"] &&
-                    titleCaseWord(user?.attributes["custom:foodPreference"])) ??
+                  {(user.attributes["custom:foodPreference"] &&
+                    titleCaseWord(user.attributes["custom:foodPreference"])) ??
                     "None"}
                 </TableCell>
               </TableRow>
             </TableBody>
           )}
         </Table>
+        {/* Display the QR Code below the table if the user is available */}
+        {user && user.attributes && (
+          <div style={{ marginTop: "1em", textAlign: "center" }}>
+            {/* The QR code value is a comma-separated string: email,sub */}
+            {user && user.attributes && (
+              <div style={{ marginTop: "1em", textAlign: "center" }}>
+                <Heading level={4}> Check-In QR Code</Heading>
+                <div
+                  style={{
+                    backgroundColor: "#fff",
+                    padding: "16px",
+                    display: "inline-block",
+                    borderRadius: "8px",
+                    boxShadow: "0 0 8px rgba(0,0,0,0.1)",
+                  }}
+                >
+                  <QRCodeSVG
+                    value={`${user.attributes.email},${user.attributes.sub}`}
+                    size={200}
+                    level="M"
+                  />
+                </div>
+                <Text marginTop="0.5em">Present this QR code at check-in.</Text>
+              </div>
+            )}
+          </div>
+        )}
       </View>
     </Flex>
   </div>
